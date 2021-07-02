c05dbeac788961b16c7fe563f0574fe5825c54ee
jovid1242 <jovid1242@gmail.com>
2021-07-02 16:48:18 +0500

    push poject

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const router = require('./router/index')
+const errorMiddlewere = require('./middlewares/errorMiddlewere')
+
+const PORT = process.env.PORT || 3010;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+app.use(errorMiddlewere);
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`)
+        })
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
